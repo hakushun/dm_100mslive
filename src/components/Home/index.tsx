9d393cc48d06529698e6a8f710c5e723b4292bc2
hakushun <hakushun.pianist@gmail.com>
2022-04-30 20:06:29 +0900

    feat: connect components to Home component

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,5 +1,19 @@
-import type { VFC } from "react";
+import { useHMSStore, selectIsConnectedToRoom, useHMSActions } from '@100mslive/react-sdk';
+import { useEffect, VFC } from 'react';
+import { VideoChat } from '../VideoChat';
+import { JoinForm } from '../JoinForm';
 
 export const Home: VFC = () => {
-  return <div>Home</div>;
+  const hmsActions = useHMSActions();
+  const isConnected = useHMSStore(selectIsConnectedToRoom);
+
+  useEffect(() => {
+    window.onunload = () => {
+      if (isConnected) {
+        hmsActions.leave();
+      }
+    };
+  }, [hmsActions, isConnected]);
+
+  return <>{isConnected ? <VideoChat /> : <JoinForm />}</>;
 };
