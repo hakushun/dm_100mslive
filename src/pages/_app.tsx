8cdde4477e1008f2049c34afb4199a1baf84df68
hakushun <hakushun.pianist@gmail.com>
2022-04-30 20:04:04 +0900

    feat: wrap react app by HMSRoomProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { HMSRoomProvider } from '@100mslive/react-sdk';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <HMSRoomProvider>
+      <Component {...pageProps} />
+    </HMSRoomProvider>
+  );
 }
 export default MyApp;
